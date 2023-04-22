30abf20f8c6d6d862fb1076392797ecaa0dbab24
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-04-22 14:26:12 -0700

    missing comments

--- src/main/java/us/malfeasant/ocrapp/sup/BadMagicException.java
+++ src/main/java/us/malfeasant/ocrapp/sup/BadMagicException.java
@@ -4,7 +4,8 @@ import us.malfeasant.ocrapp.DecodeException;
 
 public class BadMagicException extends DecodeException {
 	/**
-	 * 
+	 * Signifies an attempt to open a file that is missing its magic number.
+	 * Meaningless to try to decode the rest of the file after this.
 	 */
 	private static final long serialVersionUID = -975150291221631482L;
 
--- src/main/java/us/malfeasant/ocrapp/sup/UnknownSegmentTypeException.java
+++ src/main/java/us/malfeasant/ocrapp/sup/UnknownSegmentTypeException.java
@@ -4,7 +4,7 @@ import us.malfeasant.ocrapp.DecodeException;
 
 public class UnknownSegmentTypeException extends DecodeException {
 	/**
-	 * 
+	 * Trying to decode a segment, but the segment type is not recognized.
 	 */
 	private static final long serialVersionUID = 7128779628783885160L;
 
