c60f1c963a38b7711ebb15c65797037fc9fc7187
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-04-22 23:19:44 -0700

    not much, added reference & begun .sub class...

--- src/main/java/us/malfeasant/ocrapp/sub/ReadSUB.java
+++ src/main/java/us/malfeasant/ocrapp/sub/ReadSUB.java
@@ -1,9 +1,20 @@
 package us.malfeasant.ocrapp.sub;
 
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Reads in a .sub file- interesting to note, .idx file is generated from .sub, so not needed.
  * Seems to be more for convenience of reading & parsing by players- .sub contains everything .idx does.
+ * Reference: https://dvd.sourceforge.net/dvdinfo/index.html
  */
 public class ReadSUB {
-    
+    public ReadSUB(Path f) throws AccessDeniedException {
+        if (Files.isReadable(f)) {
+            
+        } else {
+			throw new AccessDeniedException("File " + f + " is not readable.");
+        }
+    }
 }
